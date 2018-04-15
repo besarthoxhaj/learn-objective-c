7d82de782f938a7b80bcb678c8b30faa2ed93839
Besart Shyti <besartshyti@gmail.com>
2018-04-15 13:47:11 +0100

    [feature] add types example

--- basics/types.m
+++ basics/types.m
@@ -2,7 +2,25 @@
 #include <stdio.h>
 
 int main(void) {
-  NSLog(@"Hello world.");
-  puts("Hello world.");
+
+  NSLog(@"Hello, World!");
+
+  NSString *variableString = @"Hello, World";
+  NSLog(@"Say %@!", variableString);
+
+  NSNumber *myBirthdayYear = @1990;
+  NSLog(@"Number %@!", myBirthdayYear);
+
+  // NSArray is an immutable object.
+  NSArray *people = @[@"Foo", @"Bar", @"Zoo"];
+  NSLog(@"Array %@", people[0]);
+  people = @[@"Foo", @"Bar", @"Zoo", @"Lee"];
+  NSLog(@"Array %@", people[3]);
+
+  NSArray *ratings = @[@3, @5];
+  NSDictionary *person = @{@"First Name": @"Bar"};
+  NSDictionary *appRatings = @{@"AngryFowl": @3, @"Lettertouch": @5};
+  NSLog(@"%@", appRatings[@"AngryFowl"]);
+
   return 0;
 }
